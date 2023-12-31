52f0cc733c0d0f5e48024e62e5f893d1e84b39e5
Sai Phanindra <saiphanindra265@gmail.com>
2023-12-31 19:22:41 +0530

    layout changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
     <div className="App text-3xl text-red-700">
-        Netflix GPT starter 
+       <Body />
     </div>
   );
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './Login';
+import Browse from './Browse';
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: '/browse',
+    element: <Browse /> 
+  }
+])
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
   )
 }
 
