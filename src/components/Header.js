1cdb3555be17a03bbad360722c65ad29cb57e4bd
Sai Phanindra <saiphanindra265@gmail.com>
2024-01-02 13:52:24 +0530

    login page css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
+import logo from '../assets/logo.png'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
+      <img 
+      className='w-44' 
+      src={logo} />
+    </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import login from '../assets/login.jpg';
+import { Header } from './Header';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Header />
+      <div className='absolute'>
+        <img
+          src={login}
+        />
+      </div>
+      <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0'>
+        <input type='text' placeholder='Email Address' className='p-2 my-2' />
+        <input type='password' placeholder='Password'  className='p-2 my-2' />
+        <button className='p-4 m-4'>Sign in</button>
+      </form>
+    </div>
   )
 }
 
